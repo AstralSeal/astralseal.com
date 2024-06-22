8264994c4b0c1401badab003acf483fb945d2d36
coregameHD <coregamehd@outlook.com>
2024-06-23 04:54:15 +0700

    update data + fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astralseal.pages.dev/",
+  site: "https://astralseal.com/",
   integrations: [tailwind(), icon()],
 });
