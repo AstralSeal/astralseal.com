09c77e45ce61ce35954a68c376f399eeea11e223
coregameHD <coregamehd@outlook.com>
2024-06-23 17:06:28 +0700

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astralseal.com/",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()]
 });
